d30d8f345a05958bfd7ed2a5c252334646275e22
b6320503106 <78350916+b6320503106@users.noreply.github.com>
2021-03-01 19:35:30 +0700

    Add files via upload

---
+++ Medal.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+    int n,x[3],j,i,y=0,m;
+    scanf("%d",&n);
+    if(n>3&&n<=1000)
+    {
+          for(j=0;j<n;j++)
+    {
+        for(i=0;i<3;i++)
+        scanf("%d",&x[i]);
+    }
+    for(j=0;j<3;j++)
+    {
+        for(i=0;i<3;i++)
+        y=y+x[i];
+    }
+#include<stdio.h>
+int main()
+{
+    int n,x[3],j,i,y=0,m;
+    scanf("%d",&n);
+    if(n>3&&n<=1000)
+    {
+          for(j=0;j<n;j++)
+    {
+        for(i=0;i<3;i++)
+        scanf("%d",&x[i]);
+    }
+    for(j=0;j<3;j++)
+    {
+        for(i=0;i<3;i++)
+        y=y+x[i];
+    }
+
+    }
+}
+
+
+    printf("%d %d",y/10,y%10);
+    }
+}
+
